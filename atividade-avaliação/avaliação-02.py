81a022cde27783f4654667d56defbcb63947351e
Lucas Wagner Fernandes <79675318+Lucas-WF@users.noreply.github.com>
2021-09-22 19:41:59 -0300

    Refatorando avaliação nº2

--- atividade-avaliação/avaliação-02.py
+++ atividade-avaliação/avaliação-02.py
@@ -1,63 +1,73 @@
 # -*- coding: utf-8 -*-
 """
-Preferi a utilização de dicionários, pois a "indexação" se dá pelas chaves sendo assim,
-podemos imprimir as chaves como os nomes e seus respectivos valores.
+Código refatorado da atividade-avaliação nº2
 """
-words_dict = {}
-nb_dict = {}  # Dicionário com palavras sem a barra de espaço
-longest_word = {}
-
-def insertPhrase(n):
-    lenght_word_list = []  # Lista criada para armazenar os valores do tamanho das palavras de cada frase
-    a = n.split()
+def enumerator(phrase):  # Separa o número de caracteres por -
+    word_list = []
+    a = phrase.split()
     for i in a:
-        b = len(i)
-        lenght_word_list.append(str(b))
+        word_list.append(str(len(i)))
+    enum = '-'.join(word_list)
+    return {phrase: enum}
 
-    str_a = (' - ').join(lenght_word_list)  # Método para separar os valores por -
-    words_dict[n] = str_a 
-    nbword = len(n.replace(" ",""))
-    nb_dict[n] = nbword
 
-def bigWord():  # Função para pegar a maior palavra entre todas as frases
+def no_backspace(phrase):  # Transforma a frase em uma frase sem espaçamento
+    a = phrase.replace(" ","")
+    return a
+
+
+def longest_phrase(words_dict):  # Retorna a maior frase entre todas as frases
+    z = 0
+    longest_ph = ""
+    for i, j in words_dict.items():
+        if len(j) >= z:
+            z = len(j)
+            longest_ph = i
+    return longest_ph
+
+
+def longest_word(phrase):  # Retorna a maior palavra da referida frase
+    val = 0
+    longest = ""
+    for i in phrase.split():
+        if len(i) >= val:
+            val = len(i)
+            longest = i
+    return longest
+
+
+def longest_word_phrase(words_list):  # Retorna a maior palavra entre todas as frases
     k = 0
-    for key in words_dict.keys():
-        p = key.split()
-        for i in p:
-            if len(i) >= k:
-                k = len(i)
-                longest_word = i
+    longest_word_ph = ""
+    for i in words_list:
+        for key, values in i.items():
+            p = key.split()
+            for i in p:
+                if len(i) >= k:
+                    k = len(i)
+                    longest_word_ph = i
+        return longest_word_ph
 
-    return longest_word
+
+def main():  # Função principal do código
+    words_dict = {}
+    words_list = []
+    while True:
+        op = input("Palavra: ")
+        if op == "0":  # Condição de saída como definido na questão
+            break
+        else:
+            words_dict[op] = no_backspace(op)  # Dicionário para a função
+            words_list.append(enumerator(op))  # Lista para a impressão e função
+    print("{:<25s} | {:>20s} | {:>20s}".format("Palavra de Entrada", "Quantidade de letras", "Maior Palavra"))
+    print('-' * 71)
+    for i in words_list:
+        for key, values in i.items():  # Imprimindo as frases, seus valores e suas maiores palavras separados por -
+            print("{:<25s} | {:>20s} | {:>20s}".format(key, values, longest_word(key)))
+
+    print(f"\nA maior frase entre todas as frases é: {longest_phrase(words_dict)}")
+    print(f"\nA maior palavra entre todas as frases é: {longest_word_phrase(words_list)}")
 
 
-try:
-    lenght = int(input("Quantidade de palavras que gostaria de digitar: "))
-except:
-    print("\nO valor necessita ser inteiro")
-    exit()
-
-for i in range(lenght):  # Loop para adicionar as palavras no dicionário
-    word = input("Digite algo: ")
-    insertPhrase(word)
-    print(" ")
-
-print("{:<26s} | {:>10s}".format("Palavra de Entrada", "Quantidade de letras"))
-print('-'*49)
-
-for key, values in words_dict.items():  # Imprimindo as frases e seus valores separados por -
-    print("{:<26s} | {:>10s}".format(key, values))
-    
-print('-'*49)
-print(" ")
-
-longest_word = bigWord()
-
-z = 0
-for i, j in nb_dict.items():  # Loop para pegar a maior frase entre todas as frases
-    if j >= z:
-        z = j
-        longest = i
-
-print(f"\nMaior frase ou palavra: {longest}")
-print(f"\nA maior palavra: {longest_word}")
+if __name__ == "__main__":
+    main()
